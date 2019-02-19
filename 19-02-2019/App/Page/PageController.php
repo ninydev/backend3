63c8035344d2ae7226a52f4e9d5472534b521b3b
Oleksandr Nykytin <nikitin_a@itstep.org>
2019-02-19 20:48:31 +0200

    19-02-2019 base MVC ver 2

--- 19-02-2019/App/Page/PageController.php
+++ 19-02-2019/App/Page/PageController.php
@@ -6,9 +6,20 @@ namespace App\Page;
  */
 class PageController 
 {
-	
-	function __construct(){
+	var $Model;
+	var $content;
+	function __construct($action = "none", $page_id = 0){
 		echo "Page Controller";
-		
+		if ($action == "none") return; // Нет нужного метода
+
+		$this->Model=new PageModel ();
+
+		if ($action == "show") { $this->content = $this->Model->getPageByID($page_id);}
 	}
+
+	function getContent () {return $this->content;}
+
+
+
+
 }
--- 19-02-2019/App/Page/PageModel.php
+++ 19-02-2019/App/Page/PageModel.php
@@ -9,6 +9,23 @@ class PageModel
 	
 	function __construct(){
 		echo "Page Model";
-		
 	}
+
+	function getPageByID ($id){
+		switch ($id) {
+			case '1':
+			return " Данные страницы 1 ";
+				break;
+
+			case '2':
+			return " Данные страницы 2 ";
+				break;
+			
+			default:
+			return " Нет данных "; // TODO Error 404 or HomePage
+				break;
+		}
+	}
+
+
 }
